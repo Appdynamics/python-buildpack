d8d2eb31d8929691f332203fe018ff8294cbbd5c
Pavan Krishna Nimmagadda <pavan.krishna@appdynamics.com>
2018-04-10 20:57:48 -0700

    cleanup ProcFile rewrites

--- src/python/appdynamics/appdynamics.go
+++ src/python/appdynamics/appdynamics.go
@@ -105,16 +105,3 @@ func RewriteProcFile(procFilePath string) error {
 	}
 	return nil
 }
-
-/*func main() {
-	vcapServicesStr := `{"appdynamics": [{"application-name": "app", "binding_name": null, "volume_mounts": [], "node-name": "node", "plan": "my-controller-config-name", "syslog_drain_url": null, "tier-name": "tier", "credentials": {"host-name": "442-saas-controller.e2e.appd-test.com", "plan-name": "my-controller-config-name", "guid": "c27156b4-34b0-4390-b0d8-1687fe9480c9", "plan-description": "my-controller-config-options", "ssl-enabled": false, "account-access-key": "c14b43ab-ceed-405e-8416-b028764f461b", "account-name": "customer1", "port": "8090"}, "tags": [], "label": "appdynamics", "instance_name": "1", "provider": null}]}`
-	vcapApplicationStr := `{"cf_api": "https://api.sys.pie-21.cfplatformeng.com", "application_uris": ["sample-app.cfapps.pie-21.cfplatformeng.com"], "users": null, "application_id": "98d1e9d3-cea7-47d1-83a4-987661770d1a", "name": "sample-app", "limits": {"fds": 16384}, "application_name": "sample-app", "space_name": "appdynamics-space", "space_id": "a9ba301e-d4f3-41b7-a344-d381c461ae50", "uris": ["sample-app.cfapps.pie-21.cfplatformeng.com"]}`
-	os.Setenv("VCAP_SERVICES", vcapServicesStr)
-	os.Setenv("VCAP_APPLICATION", vcapApplicationStr)
-
-	appd := GatherAppdynamicsInfo()
-	script := GenerateAppdynamicsScript(appd)
-	fmt.Println(script)
-
-	fmt.Print(GenerateStartUpCommand("web: python hello.py:8080"))
-}*/
